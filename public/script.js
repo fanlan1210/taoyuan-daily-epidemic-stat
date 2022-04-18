a10e327d4fa76a1d0dbe35a7fba7030605cdd936
Glitch (glitch-hello-website) <none>
2022-04-18 14:09:29 +0000

    🏘🎈 Checkpoint ./public/index.html:36588805/246 ./public/script.js:36588805/1247

--- public/script.js
+++ public/script.js
@@ -1,14 +1,28 @@
-fetch('/today-stat')
-  .then(function(response) {
-      if(!response.ok) throw new Error('Cannot fetch data!');
-      return response.json();
-  })
-  .then();
-
 const vm = Vue.createApp({
   data() {
     return {
-      
+      date: "00-00",
+      total: "*",
+      local: "*",
+      remote: "*",
+      taoyuan: "*"
+    }
+  },
+  methods: {
+    fetchData() {
+      fetch('/stat-today')
+      .then(function(response) {
+          if(!response.ok) throw new Error('Cannot fetch data!');
+          return response.json();
+      })
+      .then( (data)=>{
+        this.total = data.todal;
+        this.local = data.local;
+        this.remote = data.remote;
+        this.taoyuan = data.area;
+      });
     }
   }
 });
+
+vm.mount('#app');
