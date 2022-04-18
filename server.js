57b2cb697af3924d8c0fb8448351105fca167416
Glitch (glitch-hello-website) <none>
2022-04-18 11:34:13 +0000

    👨‍❤️‍👨⚾️ Checkpoint ./package.json:36588805/429 ./server.js:36588805/408

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 app.use(express.static("public"));
 
-
+app.get("/stat-today", (req, res)=>{
+  axios.get('https://www.cdc.gov.tw/RSS/RssXml/Hh094B49-DRwe2RR4eFfrQ?type=1')
+    .then( (data)=>{
+      res.set('Content-Type', 'text/xml');
+      res.send(data.data);
+    })
+    .catch( (err)=>{console.log(err);} )
+});
 
 app.listen(process.env.PORT, ()=>{
   console.log("server starting on port: " + process.env.PORT);
